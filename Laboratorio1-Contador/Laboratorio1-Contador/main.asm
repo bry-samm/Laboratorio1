cbbe0cb06dc43a60dee262cb56b265b40e274453
Bryan Morales <mor23283@uvg.edu.gt>
2025-02-04 15:26:27 -0600

    Lab1
    Configure mis pines de entrada y salida y coloqué la lógica del main loop

--- Laboratorio1-Contador/Laboratorio1-Contador/main.asm
+++ Laboratorio1-Contador/Laboratorio1-Contador/main.asm
@@ -4,7 +4,7 @@
 ; Created: 01/02/2025
 //Encabezado 
 //Bryan Samuel Morales Paredes 23283
-// Este código es un contador binario de 4 bits
+// Este código es un contador doble binario de 4 bits el cual puede generar la suma de los contadores
 .include "M328PDEF.inc"
 .cseg
 .org 0x0000
@@ -16,33 +16,34 @@
 	OUT		SPL, R16		// SPL 
 	LDI		R16, HIGH(RAMEND)
 	OUT		SPH, R16		// SPH 
-
+//=================================================================================
 //Configurar el microcontrolador (MCU)
 SETUP:
 	//Configurar pines de entrada y salida (DDRx, PORTx, PINx)
-	//Le voy a hablar a todo el puerto D y no un solo bit, por simplicidad
-	//PORTD como entrada y PORTB como salida
+	//Le voy a hablar a todo el puerto B y no un solo bit, por simplicidad
+	//PORTB como entrada
 	LDI		R16, 0x00
-	OUT		DDRD, R16	//Setear puerto D como entrada
+	OUT		DDRDB, R16	//Setear puerto B como entrada
 	LDI		R16, 0xFF
-	OUT		PORTD, R16	//Habilidar pull-up en puerto D
-	
-	//PORTB como salida inicialmente apagado 
-	LDI		R16, 0xFF
-	OUT		DDRB, R16	//Setear puerto B como salida
-	LDI		R16, 0x00
-	OUT		PORTB, R16	//Apagar puerto B
+	OUT		PORTB, R16	//Habilidar pull-up en puerto B
 
+	
+	//PORTD y PORTC como salida inicialmente apagado 
+	LDI		R16, 0xFF
+	OUT		DDRC, R16	//Setear puerto C como salida
+	OUT		DDRD, R16	//Setear puerto D como salida 
+	LDI		R16, 0x00
+	OUT		PORTC, R16	//Apagar puerto C
+	OUT		PORTD, R16	//Apagar puerto D
+//====================================================================================
+//Establecer valores iniciales de algunos registros
+
+	LDI		R21, 0x00	//Iinica el contador 2 en 0
 	LDI		R20, 0x0F	//Variable para guardar valor maximo y comparar (16)
-	LDI		R19, 0x00   //Inicializa el contador en 0, valor de contador 
+	LDI		R19, 0x00   //Inicializa el contador 1 en 0
 	LDI		R17, 0x00	//Variable para guardar estado de botones
 
-
-
-
-
 //Loop infinito (ciclo infinito)
-//Utilizaré el PIND2 y PIND3 para los botones ----- PINB0 al PINB3 para el resultado del contador (leds)
 MAIN:
  	IN		R16, PIND	//Escribe el valor de PIND en un registro
 	CP		R17, R16	//Compara los registros, salta si son diferentes
@@ -53,10 +54,21 @@ MAIN:
 	BREQ	MAIN
 	//Volver a leer PIND
 	MOV		R17, R16	//Mueve el registro actual al registro previo
-	SBRS	R16, 2		//Salta si el bit 2 de PIND (R16) está en 1
-	RJMP	REVISAR_SI	//salta a la subrutina 
-	SBRS	R16, 3		//Salta si el bit 3 de PIND está en 1
-	RJMP	REVISAR_INC_DEC
+	SBIS	PINB, 0
+	CALL	SUM_1
+	SBIS	PINB, 1
+	CALL	RESTA_1
+	SBIS	PINB, 2
+	CALL	SUM_2
+	SBIS	PINB, 3
+	CALL	RESTA_2
+	RJMP	MAIN
+
+//Sub-rutina (no de interrupción)
+
+
+
+
 
 //Sub-rutina (no de interrupción)
 REVISAR_SI:			//Sirve para la lógica cuando no se presionan botones
@@ -82,6 +94,11 @@ SET_MAX:
 ACTUALIZAR:
 	OUT		PORTB,R19	//Coloca el valor del contador en el puerto B
 	RJMP	MAIN		//Regresa al loop principal
+
+
+
+
+//=====================================================================================
 
 DELAY:		//Antirebote
 	LDI		R18, 0xFF
